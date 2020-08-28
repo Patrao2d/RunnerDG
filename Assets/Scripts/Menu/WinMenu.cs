15d366f995224241571138f6f48fafe4b9dfa33d
patrao2d <patrao2@gmail.com>
2020-08-28 15:50:19 +0100

    Amen ?

--- Assets/Scripts/Menu/WinMenu.cs
+++ Assets/Scripts/Menu/WinMenu.cs
@@ -6,10 +6,11 @@ public class WinMenu : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
     public float fadeDuration;
+    public GameObject confetis;
 
     void Start()
     {
-       
+        confetis.SetActive(true);
     }
 
     // Update is called once per frame
